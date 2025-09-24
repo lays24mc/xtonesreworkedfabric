d36646cc23694e1aae23fdcbb28dc03dc267874d
lays24mc <38819519+lays24mc@users.noreply.github.com>
2025-09-24 18:02:06 +0200

    update to 1.21.6
    fix BlockRenderLayer

--- src/main/java/net/lays24mc/xtonesreworkedfabric/XtonesReworkedFabricClient.java
+++ src/main/java/net/lays24mc/xtonesreworkedfabric/XtonesReworkedFabricClient.java
@@ -1,9 +1,10 @@
 package net.lays24mc.xtonesreworkedfabric;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
 import net.lays24mc.xtonesreworkedfabric.block.ModBlocks;
 import net.lays24mc.xtonesreworkedfabric.block.custom.GlaxxBlocks;
+import net.minecraft.client.render.BlockRenderLayer;
 import net.minecraft.client.render.RenderLayer;
 
 public class XtonesReworkedFabricClient implements ClientModInitializer {
@@ -12,24 +13,24 @@ public class XtonesReworkedFabricClient implements ClientModInitializer {
     public void onInitializeClient() {
 
         /*Make GlaxxBlocks "transparent"*/
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock1, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock2, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock3, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock4, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock5, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock6, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock7, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock8, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock9, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock10, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock11, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock12, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock13, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock14, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlaxxBlocks.GlaxxBlock15, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock1, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock2, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock3, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock4, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock5, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock6, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock7, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock8, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock9, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock10, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock11, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock12, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock13, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock14, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(GlaxxBlocks.GlaxxBlock15, BlockRenderLayer.TRANSLUCENT);
 
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.FLAT_LAMP, RenderLayer.getCutout());
+        BlockRenderLayerMap.putBlock(ModBlocks.FLAT_LAMP, BlockRenderLayer.CUTOUT);
 
     }
 }
